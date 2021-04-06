5c9c7e55ee6179f6a319234d881a831d567f391d
TATSUNO Yasuhiro <ytatsuno.jp@gmail.com>
2021-04-06 09:40:52 +0900

    Delete unused testing class
    Note) close method requires override modifier since ZooKeeper 3.7.0

--- reactive-zookeeper/src/test/scala/tanukkii/reactivezk/TestingZooKeeperMain.scala
+++
@@ -1,7 +0,0 @@
-package tanukkii.reactivezk
-
-import org.apache.zookeeper.server.ZooKeeperServerMain
-
-class TestingZooKeeperMain extends ZooKeeperServerMain {
-  def close(): Unit = shutdown()
-}
